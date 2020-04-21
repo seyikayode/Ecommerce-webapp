8c5a01d6d98d7d1db9aedbe24a24652d23b81c10
Kayode <oluwaseyikayode25@gmail.com>
2020-04-21 23:53:51 +0100

    ecommerce-webapp

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('add-coupon/', AddCoupon.as_view(), name='add-coupon'),
     path('request-refund/', RefundView.as_view(), name='request-refund'),
     path('search-results/', SearchView.as_view(), name='search'),
-    path('item/add/<slug>/', add_to_cart, name='add-to-cart'),
-    path('item/remove/<slug>/', remove_from_cart, name='remove-from-cart'),
-    path('item/add/single/<slug>/', add_single_item_to_cart, name='add-single-item-to-cart'),
-    path('item/remove/single/<slug>/', remove_single_item_from_cart, name='remove-single-item-from-cart')
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path('add-item-to-cart/<slug>/', add_single_item_to_cart, name='add-single-item-to-cart'),
+    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart, name='remove-single-item-from-cart')
 ]
